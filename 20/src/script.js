b84d3dc9b46b1ca9ce6e3227bd148fcecf6b88a7
Pedro Feltrin <pedrofeltrin45@gmail.com>
2023-06-26 21:52:18 -0300

    finish lesson 20 :))

--- 20/src/script.js
+++ 20/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import * as lil from "lil-gui";
+import gsap from "gsap";
 
 THREE.ColorManagement.enabled = false;
 
@@ -12,7 +13,9 @@ const parameters = {
   materialColor: "#ffeded",
 };
 
-gui.addColor(parameters, "materialColor");
+gui.addColor(parameters, "materialColor").onChange(() => {
+  material.color.set(parameters.materialColor);
+});
 
 /**
  * Base
@@ -24,13 +27,92 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Test cube
+ * Objects
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "#ff0000" })
+const objectDistance = 4;
+
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
+gradientTexture.magFilter = THREE.NearestFilter;
+const particleAlphaMap = textureLoader.load("/textures/disc.png");
+
+//Material
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+});
+
+// Meshes
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
 );
-scene.add(cube);
+
+scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
+
+sectionMeshes.forEach((mesh, index) => {
+  mesh.position.y = -objectDistance * index;
+  mesh.position.x = 2 * (index % 2 === 0 ? 1 : -1);
+});
+
+/**
+ * Particles
+ */
+// Geometry
+const particlesCount = 2000;
+const positions = new Float32Array(particlesCount * 3);
+const colors = new Float32Array(particlesCount * 3);
+const startColor = new THREE.Color("blue");
+const endColor = new THREE.Color("red");
+for (let i = 0; i < particlesCount; i++) {
+  const mixedColor = startColor.clone();
+  const randomY =
+    objectDistance * 0.5 -
+    Math.random() * objectDistance * sectionMeshes.length;
+  const alpha = Math.abs(randomY / (objectDistance * 3 - objectDistance * 0.5));
+  mixedColor.lerp(endColor, alpha);
+  const i3 = i * 3;
+  positions[i3] = (Math.random() - 0.5) * 10;
+  positions[i3 + 1] = randomY;
+  positions[i3 + 2] = (Math.random() - 0.5) * 10;
+
+  colors[i3] = mixedColor.r;
+  colors[i3 + 1] = mixedColor.g;
+  colors[i3 + 2] = mixedColor.b;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+const particlesMesh = new THREE.Points(
+  particlesGeometry,
+  new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    size: 0.05,
+    sizeAttenuation: true,
+    vertexColors: true,
+    alphaMap: particleAlphaMap,
+    transparent: true,
+    depthWrite: false,
+  })
+);
+scene.add(particlesMesh);
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -57,6 +139,10 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
+// Camera group
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -65,25 +151,81 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.z = 6;
-scene.add(camera);
+cameraGroup.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true,
 });
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY;
+
+  const newSection = scrollY / sizes.height;
+  if (newSection !== currentSection) {
+    currentSection = Math.floor(newSection);
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    });
+  }
+});
+
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = e.clientY / sizes.height - 0.5;
+});
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  // Animate camera
+  camera.position.y = (-scrollY / sizes.height) * objectDistance;
+
+  const parallaxX = cursor.x;
+  const parallaxY = cursor.y * -1;
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
+
+  // Animate meshes
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
+  }
 
   // Render
   renderer.render(scene, camera);
