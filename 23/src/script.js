00133f9954a115b8f3572c34aa326d28e351e69b
Pedro Feltrin <pedrofeltrin45@gmail.com>
2023-07-07 20:49:19 -0300

    learn about raycasters

--- 23/src/script.js
+++ 23/src/script.js
@@ -38,6 +38,25 @@ object3.position.x = 2;
 
 scene.add(object1, object2, object3);
 
+// object1.updateMatrixWorld();
+// object2.updateMatrixWorld();
+// object3.updateMatrixWorld();
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster();
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(10, 0, 0);
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection);
+
+// const intersect = raycaster.intersectObject(object2);
+// console.log(intersect);
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3]);
+// console.log(intersects);
+
 /**
  * Sizes
  */
@@ -58,6 +77,21 @@ window.addEventListener("resize", () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2(-1, 1);
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = (e.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(e.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener("click", (_) => {
+  clicked = currentIntersect || null;
+  console.log(clicked);
 });
 
 /**
@@ -92,8 +126,52 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const clock = new THREE.Clock();
 
+let currentIntersect = null;
+let clicked = null;
+
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+  // Cast a ray
+  // const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  // const rayDirection = new THREE.Vector3(1, 0, 0);
+  // rayDirection.normalize();
+  // raycaster.set(rayOrigin, rayDirection);
+
+  raycaster.setFromCamera(mouse, camera);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+  const intersectsIds = intersects.reduce(
+    (acc, item) => ({ ...acc, [item.object.id]: true }),
+    {}
+  );
+
+  for (const obj of objectsToTest) {
+    if (obj === clicked?.object) {
+      obj.material.color.set("#00ff00");
+    } else {
+      if (intersectsIds[obj.id]) {
+        obj.material.color.set("#0000ff");
+      } else {
+        obj.material.color.set("#ff0000");
+      }
+    }
+  }
+
+  if (intersects.length) {
+    if (!currentIntersect) {
+      console.log("mouse enter");
+      currentIntersect = intersects[0];
+    }
+  } else {
+    currentIntersect = null;
+  }
 
   // Update controls
   controls.update();
