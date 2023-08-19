ec1ee1587e488c0e412a841caf29c1d20d60df2c
Pedro Feltrin <pedrofeltrin45@gmail.com>
2023-08-19 11:38:46 -0300

    finish lesson 29

--- 29/src/shaders/test/fragment.glsl
+++ 29/src/shaders/test/fragment.glsl
@@ -10,6 +10,49 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid) {
     return vec2(cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x, cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y);
 }
 
+//	Classic Perlin 2D Noise 
+//	by Stefan Gustavson
+//
+vec2 fade(vec2 t) {
+    return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+
+vec4 permute(vec4 x) {
+    return mod(((x * 34.0) + 1.0) * x, 289.0);
+}
+
+float cnoise(vec2 P) {
+    vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+    Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+    vec4 ix = Pi.xzxz;
+    vec4 iy = Pi.yyww;
+    vec4 fx = Pf.xzxz;
+    vec4 fy = Pf.yyww;
+    vec4 i = permute(permute(ix) + iy);
+    vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+    vec4 gy = abs(gx) - 0.5;
+    vec4 tx = floor(gx + 0.5);
+    gx = gx - tx;
+    vec2 g00 = vec2(gx.x, gy.x);
+    vec2 g10 = vec2(gx.y, gy.y);
+    vec2 g01 = vec2(gx.z, gy.z);
+    vec2 g11 = vec2(gx.w, gy.w);
+    vec4 norm = 1.79284291400159 - 0.85373472095314 * vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+    g00 *= norm.x;
+    g01 *= norm.y;
+    g10 *= norm.z;
+    g11 *= norm.w;
+    float n00 = dot(g00, vec2(fx.x, fy.x));
+    float n10 = dot(g10, vec2(fx.y, fy.y));
+    float n01 = dot(g01, vec2(fx.z, fy.z));
+    float n11 = dot(g11, vec2(fx.w, fy.w));
+    vec2 fade_xy = fade(Pf.xy);
+    vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+    float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+    return 2.3 * n_xy;
+}
+
 void main() {
     /* Pattern 3 */
     // float strength = vUv.x;
@@ -62,11 +105,11 @@ void main() {
     // float strength = strength_1 + strength_2;
 
     /* Pattern 15 */
-    // float strength_1 = step(0.4, mod(vUv.x * 10.0, 1.0));
-    // strength_1 *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
-    // float strength_2 = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
-    // strength_2 *= step(0.4, mod(vUv.y * 10.0, 1.0));
-    // float strength = strength_1 + strength_2;
+    float strength_1 = step(0.4, mod(vUv.x * 10.0, 1.0));
+    strength_1 *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    float strength_2 = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    strength_2 *= step(0.4, mod(vUv.y * 10.0, 1.0));
+    float strength = strength_1 + strength_2;
 
     /* Pattern 16 */
     // float strength = abs(vUv.x - 0.5);
@@ -162,8 +205,64 @@ void main() {
     // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
     /* Pattern 40 */
-    float angle = atan(vUv.x, vUv.y);
-    float strength = angle;
+    // float angle = atan(vUv.x, vUv.y);
+    // float strength = angle;
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
+    /* Pattern 41 */
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // float strength = angle;
+
+    /* Pattern 42 */
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle += PI;
+    // angle /= PI * 2.0;
+    // float strength = angle;
+
+    /* Pattern 43 */
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle += PI;
+    // angle /= PI * 2.0;
+    // angle *= 20.0;
+    // angle = mod(angle, 1.0);
+    // float strength = angle;
+
+    /* Pattern 44 */
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle += PI;
+    // angle /= PI * 2.0;
+    // float strength = sin(angle * 100.0);
+
+    /* Pattern 45 */
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle += PI;
+    // angle /= PI * 2.0;
+    // float sinusoid = sin(angle * 100.0);
+    // float radius = 0.25 + sinusoid * 0.02;
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+
+    /* Pattern 46 */
+    // float strength = cnoise(vUv * 50.0);
+
+    /* Pattern 47 */
+    // float strength = step(0.0, cnoise(vUv * 10.0));
+
+    /* Pattern 48 */
+    // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+
+    /* Pattern 49 */
+    // float strength = sin(cnoise(vUv * 10.0) * 20.0);
+
+    /* Pattern 50 */
+    // float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+
+    // Clamp strength
+    strength = clamp(strength, 0.0, 1.0);
+
+    /* Colored version */
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    gl_FragColor = vec4(mixedColor, 1.0);
+
+    // gl_FragColor = vec4(vec3(strength), 1.0);
 }
