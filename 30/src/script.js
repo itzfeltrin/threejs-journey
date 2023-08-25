93678e614c66d3bdb319df13d40e35d688aaff22
Pedro Feltrin <pedrofeltrin45@gmail.com>
2023-08-25 09:30:57 -0300

    finish ragin sea

--- 30/src/script.js
+++ 30/src/script.js
@@ -12,8 +12,8 @@ THREE.ColorManagement.enabled = false;
 // Debug
 const gui = new lil.GUI({ width: 340 });
 const debugObject = {};
-debugObject.depthColor = "#0000ff";
-debugObject.surfaceColor = "#8888ff";
+debugObject.depthColor = "#186691";
+debugObject.surfaceColor = "#9bd8ff";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -25,7 +25,7 @@ const scene = new THREE.Scene();
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
 
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
@@ -38,10 +38,15 @@ const waterMaterial = new THREE.ShaderMaterial({
     uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
     uBigWavesSpeed: { value: 0.75 },
 
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallIterations: { value: 4.0 },
+
     uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
     uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
-    uColorOffset: { value: 0.25 },
-    uColorMultiplier: { value: 2 },
+    uColorOffset: { value: 0.08 },
+    uColorMultiplier: { value: 5 },
   },
 });
 waterMaterial.side = THREE.DoubleSide;
@@ -89,6 +94,30 @@ gui
   .max(10)
   .step(0.001)
   .name("uColorMultiplier");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSmallWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name("uSmallWavesFrequency");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uSmallWavesSpeed");
+gui
+  .add(waterMaterial.uniforms.uSmallIterations, "value")
+  .min(0)
+  .max(15)
+  .step(1)
+  .name("uSmallIterations");
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial);
--- 30/src/shaders/water/vertex.glsl
+++ 30/src/shaders/water/vertex.glsl
@@ -2,6 +2,10 @@ uniform float uTime;
 uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
 uniform float uBigWavesSpeed;
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
 
 varying float vElevation;
 
@@ -90,8 +94,11 @@ void main() {
 	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
 	/* Elevation */
-	float elevation = +sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * uBigWavesElevation;
-	elevation += cnoise(vec3(modelPosition.xz * 10.0, uTime * 0.2));
+	float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * uBigWavesElevation;
+
+	for(float i = 1.0; i <= uSmallIterations; i++) {
+		elevation -= abs(cnoise(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+	}
 
 	modelPosition.y += elevation;
 
