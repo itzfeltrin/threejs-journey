9ba4353b3614b362961d6f2426a5d3077371de1a
Pedro Feltrin <pedrofeltrin45@gmail.com>
2023-09-25 20:07:48 -0300

    finish lesson 32

--- 32/src/script.js
+++ 32/src/script.js
@@ -93,12 +93,20 @@ material.onBeforeCompile = (shader) => {
     `
   );
   shader.vertexShader = shader.vertexShader.replace(
+    "#include <beginnormal_vertex>",
+    `
+      #include <beginnormal_vertex>
+    
+      float angle = (position.y + uTime) * 0.9;
+      mat2 rotateMatrix = get2dRotateMatrix(angle);
+    
+      objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  );
+  shader.vertexShader = shader.vertexShader.replace(
     "#include <begin_vertex>",
     `
       #include <begin_vertex>
-      
-      float angle = position.y + uTime * 0.9;
-      mat2 rotateMatrix = get2dRotateMatrix(angle);
       
       transformed.xz = rotateMatrix * transformed.xz;
     `
@@ -125,7 +133,7 @@ depthMaterial.onBeforeCompile = (shader) => {
     `
       #include <begin_vertex>
       
-      float angle = position.y + uTime * 0.9;
+      float angle = (position.y + uTime) * 0.9;
       mat2 rotateMatrix = get2dRotateMatrix(angle);
       
       transformed.xz = rotateMatrix * transformed.xz;
